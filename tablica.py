61eb41717d1c81b15f30fd09bbd3ad2ea4355edc
Fr3gR <85771704+Fr3gR@users.noreply.github.com>
2023-03-10 15:49:37 +0100

    Add files via upload

--- tablica.py
+++ tablica.py
@@ -1,6 +1,9 @@
 tab = [5, 3, 6, 10, 9, 7, 1, 2, 4, 8]
 
-n=len(tab)
+n = len(tab)
 for i in range(n):
-    for j in range(i+1,n):
-        print(i,j)
+    for j in range(i+1, n):
+        if tab[i] > tab [j]:
+            bufor = i
+            i = j
+            j = bufor
